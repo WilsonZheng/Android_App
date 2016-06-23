c2bc8b02319715d412e6aff9259e8e4b4512a800
Wilson Zheng <zhwzhw29@gmail.com>
2016-06-23 13:35:29 +1200

    App with different types of tabs with menu bar (icon only,icon and text,text only)

---
+++ MaterialTabs/app/src/main/java/info/androidhive/materialtabs/activity/MainActivity.java
@@ -0,0 +1,146 @@
+package info.androidhive.materialtabs.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import info.androidhive.materialtabs.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private Button btnSimpleTabs, btnScrollableTabs, btnIconTextTabs, btnIconTabs, btnCustomIconTextTabs;
+    private FloatingActionButton mFab;
+    private DrawerLayout mDrawer;
+    private NavigationView mNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //FloatingActionButton
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //Drawer Nav
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+        //View navHeaderView = mNavigationView..inflateHeaderView(R.layout.nav_header_main);
+        TextView tvFullname = (TextView) mNavigationView.findViewById(R.id.tv_full_name);
+        TextView tvUsername = (TextView) mNavigationView.findViewById(R.id.tv_username);
+
+        //Get user profile from remote server or local storage
+        tvFullname.setText("Kai");
+        tvUsername.setText("kai.zhang@centralstationsoftware.co.nz");
+
+        btnSimpleTabs = (Button) findViewById(R.id.btnSimpleTabs);
+        btnScrollableTabs = (Button) findViewById(R.id.btnScrollableTabs);
+        btnIconTextTabs = (Button) findViewById(R.id.btnIconTextTabs);
+        btnIconTabs = (Button) findViewById(R.id.btnIconTabs);
+        btnCustomIconTextTabs = (Button) findViewById(R.id.btnCustomIconTabs);
+
+        btnSimpleTabs.setOnClickListener(this);
+        btnScrollableTabs.setOnClickListener(this);
+        btnIconTextTabs.setOnClickListener(this);
+        btnIconTabs.setOnClickListener(this);
+        btnCustomIconTextTabs.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSimpleTabs:
+                startActivity(new Intent(MainActivity.this, SimpleTabsActivity.class));
+                break;
+            case R.id.btnScrollableTabs:
+                startActivity(new Intent(MainActivity.this, ScrollableTabsActivity.class));
+                break;
+            case R.id.btnIconTextTabs:
+                startActivity(new Intent(MainActivity.this, IconTextTabsActivity.class));
+                break;
+            case R.id.btnIconTabs:
+                startActivity(new Intent(MainActivity.this, IconTabsActivity.class));
+                break;
+            case R.id.btnCustomIconTabs:
+                startActivity(new Intent(MainActivity.this, CustomViewIconTextTabsActivity.class));
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
